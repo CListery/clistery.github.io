dc4f02a11f5a3fa3209b277f054676eef360b19a
CListery <cai1083088795@gmail.com>
2025-01-20 08:45:45 +0000

    Deploy by source

--- js/plugins/typed.js
+++ js/plugins/typed.js
@@ -1,1 +1,70 @@
-let config={usrTypeSpeed:theme.home_banner.subtitle.typing_speed,usrBackSpeed:theme.home_banner.subtitle.backing_speed,usrBackDelay:theme.home_banner.subtitle.backing_delay,usrStartDelay:theme.home_banner.subtitle.starting_delay,usrLoop:theme.home_banner.subtitle.loop,usrSmartBackspace:theme.home_banner.subtitle.smart_backspace,usrHitokotoAPI:theme.home_banner.subtitle.hitokoto.api,usrShowCursor:theme.home_banner.subtitle.show_cursor};export default function initTyped(t){let{usrTypeSpeed:o,usrBackSpeed:r,usrBackDelay:a,usrStartDelay:s,usrLoop:n,usrSmartBackspace:h,usrHitokotoAPI:e,usrShowCursor:u}=config;var i;function m(e){new Typed("#"+t,{strings:[e],typeSpeed:o||100,smartBackspace:h||!1,backSpeed:r||80,backDelay:a||1500,loop:n||!1,startDelay:s||500,showCursor:u||!1})}theme.home_banner.subtitle.typed&&(theme.home_banner.subtitle.hitokoto.enable?fetch(e).then(e=>e.json()).then(e=>{e.from_who&&theme.home_banner.subtitle.hitokoto.show_author?m(e.hitokoto+"——"+e.from_who):m(e.hitokoto)}).catch(console.error):(i=[...theme.home_banner.subtitle.text],document.getElementById(t)&&new Typed("#"+t,{strings:i,typeSpeed:o||100,smartBackspace:h||!1,backSpeed:r||80,backDelay:a||1500,loop:n||!1,startDelay:s||500,showCursor:u||!1})))}export{config};
+/*
+author: @jiangwen5945 & EvanNotFound
+*/
+export const config = {
+  usrTypeSpeed: theme.home_banner.subtitle.typing_speed,
+  usrBackSpeed: theme.home_banner.subtitle.backing_speed,
+  usrBackDelay: theme.home_banner.subtitle.backing_delay,
+  usrStartDelay: theme.home_banner.subtitle.starting_delay,
+  usrLoop: theme.home_banner.subtitle.loop,
+  usrSmartBackspace: theme.home_banner.subtitle.smart_backspace,
+  usrHitokotoAPI: theme.home_banner.subtitle.hitokoto.api,
+  usrShowCursor: theme.home_banner.subtitle.show_cursor,
+};
+
+export default function initTyped(id) {
+  const {
+    usrTypeSpeed,
+    usrBackSpeed,
+    usrBackDelay,
+    usrStartDelay,
+    usrLoop,
+    usrSmartBackspace,
+    usrHitokotoAPI,
+    usrShowCursor,
+  } = config;
+
+  if (!theme.home_banner.subtitle.typed) {
+    return
+  }
+
+  function typing(dataList) {
+    const st = new Typed("#" + id, {
+      strings: [dataList],
+      typeSpeed: usrTypeSpeed || 100,
+      smartBackspace: usrSmartBackspace || false,
+      backSpeed: usrBackSpeed || 80,
+      backDelay: usrBackDelay || 1500,
+      loop: usrLoop || false,
+      startDelay: usrStartDelay || 500,
+      showCursor: usrShowCursor || false,
+    });
+  }
+
+  if (theme.home_banner.subtitle.hitokoto.enable) {
+    fetch(usrHitokotoAPI)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.from_who && theme.home_banner.subtitle.hitokoto.show_author) {
+          typing(data.hitokoto + "——" + data.from_who);
+        } else {
+          typing(data.hitokoto);
+        }
+      })
+      .catch(console.error);
+  } else {
+    const sentenceList = [...theme.home_banner.subtitle.text];
+    if (document.getElementById(id)) {
+      const st = new Typed("#" + id, {
+        strings: sentenceList,
+        typeSpeed: usrTypeSpeed || 100,
+        smartBackspace: usrSmartBackspace || false,
+        backSpeed: usrBackSpeed || 80,
+        backDelay: usrBackDelay || 1500,
+        loop: usrLoop || false,
+        startDelay: usrStartDelay || 500,
+        showCursor: usrShowCursor || false,
+      });
+    }
+  }
+}
